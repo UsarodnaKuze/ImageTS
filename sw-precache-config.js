88f054869abd11e68f06689e56796a56d0791c43
aczeh <andras.czeh@atollgroup.eu>
2017-12-07 21:05:38 +0100

    change url on github remove unused buttons

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -8,7 +8,7 @@ module.exports = {
     'dist/**.css'
   ],
   runtimeCaching: [{
-    urlPattern: /^https:\/\/usarodnakuze\.github\.io\/api/,
+    urlPattern: /^https:\/\/andrasczeh\.github\.io/,
     handler: 'networkFirst'
   }, {
     urlPattern: /\/assets\//,
